c9d9a6c81e558df853fc88bfd8fc5b6e66e910e6
bfboarder <d.vanwijck@upcmail.nl>
2021-03-01 12:54:33 +0100

    HW update

--- ESP32LapTimer/src/HardwareConfig.cpp
+++ ESP32LapTimer/src/HardwareConfig.cpp
@@ -24,8 +24,8 @@ uint8_t CS_PINS[MAX_NUM_RECEIVERS] = {CS1, CS2, CS3, CS4, CS5, CS6};
 
 void InitHardwarePins() {
 
-  pinMode(SCK, OUTPUT);
-  pinMode(MOSI, OUTPUT);
+  pinMode(VRX_SCK, OUTPUT);
+  pinMode(VRX_MOSI, OUTPUT);
   for(int i = 0; i < MAX_NUM_RECEIVERS; i++) {
     pinMode(CS_PINS[i], OUTPUT);
     digitalWrite(CS_PINS[i], HIGH);
